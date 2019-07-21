1e4f93f39b574a0f5dfbf8b3cd630f34487d4bfc
badarithm <karolis.markevicius@protonmail.ch>
2019-07-21 23:23:13 +0200

    product model + services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {Product} from './models/Product';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: '<h1>All Products</h1>' +
+    '<ul>' +
+    '<li *ngFor="let product of products">' +
+    '{{product.title}}' +
+    '</li>' +
+    '</ul>',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  products: Array<Product> = [];
   title = 'angular-with-typescript';
 }
---
+++ src/app/models/Product.ts
@@ -0,0 +1,4 @@
+
+export class Product {
+  public title: string;
+}
